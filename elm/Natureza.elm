dd1f5fc7a9fc1b8f34e2b2e3093ff2ed790052ac
Sal <physicsfu@gmail.com>
2014-07-27 00:21:59 -0400

    0n0n1

--- elm/Natureza.elm
+++ elm/Natureza.elm
@@ -1,22 +1,50 @@
-import Window
+module Natureza0d0d1 where
+import Debug (log)
+import Array (Array, repeat, getOrFail, initialize, toList)
+import List (foldl, map, (++))
+import Graphics.Collage (Form, collage, toForm, move)
+import Graphics.Element (Element, image)
 
---- MODEL ---
-type TileBase = {
-  src       : String,
-  x         : Int,
-  y         : Int,
-  onGround  : [Entity],
-  items     : [Item],
-  occupying : Entity,
-  space     : Int
+-- Model
+
+mapLen : Int
+mapLen = 5
+iOffset : Int
+iOffset = div mapLen 2
+imgSize : Int
+imgSize = 100
+size : Int
+size = mapLen*imgSize
+
+type Tile = {
+  src : String
 }
 
-type EntityBase = {
-  src       : String,
-  x         : Int,
-  y         : Int
-}
+emptyTile : Tile
+emptyTile = { src = "http://mysterymachine.github.io/images/blanktile0d0d1.gif" }
 
-data Entity
+tmap : Array (Array Tile)
+tmap = repeat mapLen (repeat mapLen emptyTile)
 
-type Map = [[Tile]]
+-- View
+atPos : (Int, Int) -> (Float, Float)
+atPos (x, y) = (toFloat((x-iOffset)*imgSize), toFloat((iOffset- y)*imgSize))
+
+drawMap : Array(Array Tile) -> [Form]
+drawMap tmap = let 
+    range = (toList (initialize mapLen (\n -> n)))
+    drawTile : Int -> Int -> Tile -> Form
+    drawTile x y t = move (atPos (x, y)) <| toForm <| image imgSize imgSize t.src
+    
+    selectAndDrawTile : Int -> Int -> Form
+    selectAndDrawTile x y = drawTile x y <| getOrFail x <| getOrFail y tmap
+
+    drawRowOfTilesFromArray : Int -> [Form]
+    drawRowOfTilesFromArray x = map (selectAndDrawTile x) range
+  in
+   foldl (++) [] <| map drawRowOfTilesFromArray range
+
+view : Element 
+view = collage size size <| drawMap tmap
+
+main = view
