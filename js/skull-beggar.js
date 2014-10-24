59fe8b9a6eb245011ce7ea210aa7d4a961bb7942
Sal <physicsfu@gmail.com>
2014-10-24 01:50:42 -0400

    Channel creation and loading

--- js/skull-beggar.js
+++ js/skull-beggar.js
@@ -1,15 +1,31 @@
 var app = angular.module('skullBeggar', ['ngResource']);
 
-app.service('skullApi', function($resource){
+app.service('skullApi', function($resource, $location){
   var root = "http://localhost:3000";
   
-  var loadUserCallbackPass = function(response){
+  var loadUserPass = function(response){
     this.$scope.user = response;
+    if(!this.$scope.channelId){
+      this.Channel.create({}, createChannelPass.bind(this), loadChannelFail.bind(this));
+    }
   };
   
-  var loadUserCallbackFail = function(response){
+  var loadUserFail = function(response){
     this.Omniauth.authorize();
-  }
+  };
+  
+  var loadChannelPass = function(response){
+    this.$scope.channel = response;
+  };
+  
+  var loadChannelFail = function(response){
+    this.$scope.criticalError = true;
+  };
+  
+  var createChannelPass = function(response){
+    this.$scope.channel = response;
+    $location.search("channel_id", response.id);
+  };
   
   var api = {
     Omniauth: {
@@ -20,28 +36,41 @@ app.service('skullApi', function($resource){
     User: $resource(root + "/user/:id.json", {id: '@id'}, {
       'get': { method: 'GET', withCredentials: true }
     }),
-    ChannelAccount: $resource(root + "/channel_account/:id", {id: '@id'}, {
+    ChannelAccount: $resource(root + "/channel_account/:id.json", {id: '@id'}, {
       'get': { method: 'GET' },
-      'setInactive': { method: 'POST', url: root + "/channel_account/:id/set_inactive.json", withCredentials: true },
-      'openBetting': { method: 'POST', url: root + "/channel_account/:id/open_betting.json", withCredentials: true },
-      'closeBetting': { method: 'POST', url: root + "/channel_account/:id/close_betting.json", withCredentials: true },
-      'completeBetting': { method: 'POST', url: root + "/channel_account/:id/complete_betting.json", withCredentials: true }
+      'setInactive': { method: 'POST', url: root + "/channel_accounts/:id/set_inactive.json", withCredentials: true },
+      'openBetting': { method: 'POST', url: root + "/channel_accounts/:id/open_betting.json", withCredentials: true },
+      'closeBetting': { method: 'POST', url: root + "/channel_accounts/:id/close_betting.json", withCredentials: true },
+      'completeBetting': { method: 'POST', url: root + "/channel_accounts/:id/complete_betting.json", withCredentials: true }
     }),
-    Channel: $resource(root + "/channel/:id", {id: '@id'},{
+    Channel: $resource(root + "/channels/:id.json", {id: '@id'},{
+      'create': { method: 'POST', url: root + "/channels.json", withCredentials: true },
       'get': { method: 'GET', withCredentials: true },
-      'rest': { method: 'POST', url: root + "/channel/:id/rest.json", withCredentials: true },
-      'donateBlood': { method: 'POST', url: root + "/channel/:id/donate_blood.json", withCredentials: true },
-      'bet': { method: 'POST', url: root + "/channel/:id/bet.json", withCredentials: true }
+      'rest': { method: 'POST', url: root + "/channels/:id/rest.json", withCredentials: true },
+      'donateBlood': { method: 'POST', url: root + "/channels/:id/donate_blood.json", withCredentials: true },
+      'bet': { method: 'POST', url: root + "/channels/:id/bet.json", withCredentials: true }
     }),
     load: function($scope){
+      var cid = $location.search()["channel_id"];
+      if(angular.isString(cid)){
+        $scope.channelId = cid;
+      }
       this.$scope = $scope;
-      this.User.get({}, loadUserCallbackPass.bind(this), loadUserCallbackFail.bind(this));
+      this.User.get({}, loadUserPass.bind(this), loadUserFail.bind(this));
+      if($scope.channelId){
+        this.Channel.get({id: $scope.channelId}, 
+          loadChannelPass.bind(this), 
+          loadChannelFail.bind(this))
+      }
     }
   }
   
   return api;
 });
 
-app.controller('skullController', function($scope, skullApi){
+app.controller('skullController', function($scope, $location, skullApi){
   skullApi.load($scope);
 });
+
+
+
