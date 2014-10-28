89e89d08b5d090e5b80d2b033c6cb5f84fb6ca3a
Sal <physicsfu@gmail.com>
2014-10-28 01:10:33 -0400

    Refactor that JS

--- js/skull-beggar.js
+++ js/skull-beggar.js
@@ -3,20 +3,22 @@ var app = angular.module('skullBeggar', ['ngResource']);
 app.service('skullApi', function($resource, $location){
   var root = "http://localhost:3000";
   
+  var api = {};
+  
   var setOwner = function(){
     if(this.$scope.loaded){
       this.$scope.owner = 
         this.$scope.user.channel_id === this.$scope.channel.id;
     }  
-  };
+  }.bind(api);
   
   var setChannelDataIntoScope = function(){
-    this.$scope.loaded = loaded.call(this);
+    this.$scope.loaded = loaded();
     this.$scope.streamUrl = 
       "http://www.twitch.tv/" + this.$scope.channel.name;
     this.$scope.streamerName = this.$scope.channel.display_name;
-    setOwner.call(this);
-  };
+    setOwner();
+  }.bind(api);
   
   // Load the resource into the scope. If the page has
   // no channel_id search, try to redirect to the user's
@@ -24,37 +26,37 @@ app.service('skullApi', function($resource, $location){
   // redirecting
   var loadUserPass = function(response){
     this.$scope.user = response;
-    this.$scope.loaded = loaded.call(this);
-    setOwner.call(this);
+    this.$scope.loaded = loaded();
+    setOwner();
     
     if(!this.$scope.channelName){
       if(this.$scope.user.channel_id){
-        loadUsersChannel.call(this);
+        loadUsersChannel();
       }
       else{
-        createAndLoadUsersChannel.call(this);
+        createAndLoadUsersChannel();
       }
     }
-  };
+  }.bind(api);
   
   var loadUserFail = function(response){
     this.Omniauth.authorize();
-  };
+  }.bind(api);
   
   var loadChannelPass = function(response){
     this.$scope.channel = response;
-    setChannelDataIntoScope.call(this);
-  };
+    setChannelDataIntoScope();
+  }.bind(api);
   
   var loadChannelFail = function(response){
     this.$scope.criticalError = true;
-  };
+  }.bind(api);
   
   var createChannelPass = function(response){
     this.$scope.channel = response;
-    setChannelDataIntoScope.call(this);
+    setChannelDataIntoScope();
     $location.search("channel", response.name);
-  };
+  }.bind(api);
   
   // If the channel_id is in the search, load it into the scope
   var loadChannelName = function(){
@@ -62,69 +64,71 @@ app.service('skullApi', function($resource, $location){
     if(angular.isString(name)){
       this.$scope.channelName = name;
     } 
-  };
+  }.bind(api);
   
   // If the load passes, just load resource into the scope,
   // otherwise it is a critical failure
   var loadChannel = function(){
     this.Channel.get({name: this.$scope.channelName}, 
-      loadChannelPass.bind(this), 
-      loadChannelFail.bind(this));
-  };
+      loadChannelPass, 
+      loadChannelFail);
+  }.bind(api);
   
   var loadUsersChannel = function(){
     $location.search("channel", this.$scope.user.name);
-  };
+  }.bind(api);
   
   // If the creation works, redirect to that channel page, otherwise
   // it is a critical failure
   var createAndLoadUsersChannel = function(){
-    this.Channel.create({}, createChannelPass.bind(this), loadChannelFail.bind(this));
-  };
+    this.Channel.create({}, createChannelPass, loadChannelFail);
+  }.bind(api);
   
   // User callbacks will attempt to query the Twitch API if the user is
   // not found, and will create/redirect to the User's channel if there
   // is no channel_id in scope
   var loadUser = function(){
-    this.User.get({}, loadUserPass.bind(this), loadUserFail.bind(this));
-  };
+    this.User.get({}, loadUserPass, loadUserFail);
+  }.bind(api);
   
   var loaded = function(){
     return this.$scope.user && this.$scope.channel;
+  }.bind(api);
+  
+  api["Omniauth"] = {
+    authorize: function(){
+      window.location.href = root + "/users/auth/twitch";
+    }
   };
   
-  var api = {
-    Omniauth: {
-      authorize: function(){
-        window.location.href = root + "/users/auth/twitch";
-      }
-    },
-    User: $resource(root + "/user/:id.json", {id: '@id'}, {
-      'get': { method: 'GET', withCredentials: true }
-    }),
-    ChannelAccount: $resource(root + "/channel_account/:id.json", {id: '@id'}, {
-      'get': { method: 'GET' },
-      'setInactive': { method: 'POST', url: root + "/channel_accounts/:id/set_inactive.json", withCredentials: true },
-      'openBetting': { method: 'POST', url: root + "/channel_accounts/:id/open_betting.json", withCredentials: true },
-      'closeBetting': { method: 'POST', url: root + "/channel_accounts/:id/close_betting.json", withCredentials: true },
-      'completeBetting': { method: 'POST', url: root + "/channel_accounts/:id/complete_betting.json", withCredentials: true }
-    }),
-    Channel: $resource(root + "/channels/:id.json", {id: '@id'},{
-      'create': { method: 'POST', url: root + "/channels.json", withCredentials: true },
-      'get': { method: 'GET', url: root + "/channels/:name.json", withCredentials: true },
-      'rest': { method: 'POST', url: root + "/channels/:id/rest.json", withCredentials: true },
-      'donateBlood': { method: 'POST', url: root + "/channels/:id/donate_blood.json", withCredentials: true },
-      'bet': { method: 'POST', url: root + "/channels/:id/bet.json", withCredentials: true }
-    }),
-    // Does all the initial backend queries
-    initialize: function($scope){
-      this.$scope = $scope;
-      
-      loadChannelName.call(this);
-      if($scope.channelName){ loadChannel.call(this); }
-      loadUser.call(this);
-    }
-  }
+  api["User"] = $resource(root + "/user/:id.json", {id: '@id'}, {
+    'get': { method: 'GET', withCredentials: true }
+  });
+  
+  api["ChannelAccount"] = $resource(root + "/channel_account/:id.json", {id: '@id'}, {
+    'get': { method: 'GET' },
+    'setInactive': { method: 'POST', url: root + "/channel_accounts/:id/set_inactive.json", withCredentials: true },
+    'openBetting': { method: 'POST', url: root + "/channel_accounts/:id/open_betting.json", withCredentials: true },
+    'closeBetting': { method: 'POST', url: root + "/channel_accounts/:id/close_betting.json", withCredentials: true },
+    'completeBetting': { method: 'POST', url: root + "/channel_accounts/:id/complete_betting.json", withCredentials: true }
+  });
+  
+  api["Channel"] = $resource(root + "/channels/:id.json", {id: '@id'},{
+    'create': { method: 'POST', url: root + "/channels.json", withCredentials: true },
+    'get': { method: 'GET', url: root + "/channels/:name.json", withCredentials: true },
+    'rest': { method: 'POST', url: root + "/channels/:id/rest.json", withCredentials: true },
+    'donateBlood': { method: 'POST', url: root + "/channels/:id/donate_blood.json", withCredentials: true },
+    'bet': { method: 'POST', url: root + "/channels/:id/bet.json", withCredentials: true }
+  });
+  
+  // Does all the initial backend queries
+  api["initialize"] = function($scope){
+    this.$scope = $scope;
+    
+    loadChannelName();
+    if($scope.channelName){ loadChannel(); }
+    loadUser();
+  };
   
   return api;
 });
